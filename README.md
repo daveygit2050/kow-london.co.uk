b427ca198d3c2ea4330e761116e865da40fe1c9f
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2022-02-14 10:51:35 +0000

    Implement vite for static content building
    - Instead of just pulling js scripts from a CDN, use npm and vite to package them.
    - Move frontned content into a `static` directory.
    - Separate javascript out of `index.html`.
    - Add boilerplate css style.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # kow.london
+
+Resources for kow.london.
+
+## Development
+
+### Static
+
+For local development:
+
+```
+cd static
+npm start
+```
+
+Browse to http://localhost:3000.
---
+++ static/main.js
@@ -0,0 +1,85 @@
+import './style.css';
+import { Circle, Fill, Style } from 'ol/style';
+import { Feature, Map, Overlay, View } from 'ol/index';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Point } from 'ol/geom';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { useGeographic } from 'ol/proj';
+
+useGeographic();
+const place = [-1.21, 52.9976600];
+const point = new Point(place);
+
+var map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        features: [new Feature(point)],
+      }),
+      style: new Style({
+        image: new Circle({
+          radius: 12,
+          fill: new Fill({ color: 'red' }),
+        }),
+      }),
+    }),
+  ],
+  view: new View({
+    center: place,
+    zoom: 8
+  })
+});
+
+const element = document.getElementById('popup');
+
+const popup = new Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [0, -10],
+});
+map.addOverlay(popup);
+
+function formatCoordinate(coordinate) {
+  return "Mantic HQ";
+}
+
+// const info = document.getElementById('info');
+// map.on('moveend', function () {
+//   const view = map.getView();
+//   const center = view.getCenter();
+//   info.innerHTML = formatCoordinate(center);
+// });
+
+map.on('click', function (event) {
+  $(element).popover('dispose');
+
+  const feature = map.getFeaturesAtPixel(event.pixel)[0];
+  if (feature) {
+    const coordinate = feature.getGeometry().getCoordinates();
+    popup.setPosition([
+      coordinate[0] + Math.round(event.coordinate[0] / 360) * 360,
+      coordinate[1],
+    ]);
+    $(element).popover({
+      container: element.parentElement,
+      html: true,
+      sanitize: false,
+      content: formatCoordinate(coordinate),
+      placement: 'top',
+    });
+    $(element).popover('show');
+  }
+});
+
+map.on('pointermove', function (event) {
+  if (map.hasFeatureAtPixel(event.pixel)) {
+    map.getViewport().style.cursor = 'pointer';
+  } else {
+    map.getViewport().style.cursor = 'inherit';
+  }
+});
