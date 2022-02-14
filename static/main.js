67f6151b40fae77dc12bf8aced797ef5b4cf23ad
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2022-02-14 15:14:46 +0000

    Manually import event data into json
    This won't scale well, but will do for now.

--- static/main.js
+++ static/main.js
@@ -9,8 +9,57 @@ import { useGeographic } from 'ol/proj';
 useGeographic();
 
 const venues = {
-  "-1.21, 52.99766": { "name": "Mantic HQ"},
-  "-0.1704406, 51.325716": { "name": "Stane of Blood" }
+  "-1.207636751927884, 52.998202635714286": {
+    "name": "Clash of Kings Doubles"
+  },
+  "-0.7519496, 51.6314081": {
+    "name": "Unbroken Oaths"
+  },
+  "-1.665849975, 53.81729505": {
+    "name": "Leodis Bash 2022"
+  },
+  "-1.99111, 50.73811": {
+    "name": "Entoyment - Slay Bells Ring"
+  },
+  "-2.7095, 53.77466": {
+    "name": "Easter Beaster"
+  },
+  "-2.51603, 52.699": {
+    "name": "Rift Wars 22: Spring"
+  },
+  "-3.9393794, 56.1240295": {
+    "name": "King beyond the wall 7: The Wildlings GT"
+  },
+  "-0.28374, 51.66798": {
+    "name": "Kings of Herts X at The Pit"
+  },
+  "-3.20003081426867, 51.47220274901428": {
+    "name": "Clash of Kings Welsh Qualifier 2022"
+  },
+  "0.14987444167088593, 51.44186635": {
+    "name": "Shroud of the Reaper - Clash of the Titans"
+  },
+  "-0.45878109791520494, 51.771704575": {
+    "name": "Kings of Herts XII"
+  },
+  "-1.578262363011861, 53.67890943333333": {
+    "name": "Northern Kings GT 2022"
+  },
+  "-1.37817, 52.53923": {
+    "name": "Dawn of War The Finale"
+  },
+  "-1.1508377599211634, 52.957756919999994": {
+    "name": "Swords of Summer - God's Descend"
+  },
+  "-1.24032, 52.78608": {
+    "name": "Battle Masters 2022"
+  },
+  "-0.03420119273410058, 51.63167256666667": {
+    "name": "Clash of Kings 22"
+  },
+  "-0.7621570470502979, 51.276581300000004": {
+    "name": "Kings of Warfare"
+  }
 };
 
 var mapLayers = [new TileLayer({
@@ -64,13 +113,6 @@ function getGridRefArray(gridRefString) {
 function getGridRefString(gridRefArray) {
   return `${gridRefArray[0]}, ${gridRefArray[1]}`
 }
-
-// const info = document.getElementById('info');
-// map.on('moveend', function () {
-//   const view = map.getView();
-//   const center = view.getCenter();
-//   info.innerHTML = formatCoordinate(center);
-// });
 
 map.on('click', function (event) {
   $(element).popover('dispose');
