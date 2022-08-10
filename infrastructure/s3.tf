5c84256a2700e1c239bf78089963b5a910033fcb
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2022-08-10 22:12:14 +0100

    Migrate state to S3

---
+++ infrastructure/s3.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "state" {
+  bucket = "kow-london.co.uk-state"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,8 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket = "kow-london.co.uk-state"
+    key    = "tfstate"
+    region = "eu-west-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.25.0"
     }
   }
 }
